de314c33fbcc989b3d19795c8803112b3e39dcff
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2024-12-08 18:28:17 -0800

    Read to Patient done and active, maybe buggy

---
+++ 1.5/Source/Medical_Books/JobDriver_PatientListen.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace Medical_Books
+{
+    public class JobDriver_PatientListen : JobDriver_LayDownAwake
+    {
+        public override bool LookForOtherJobs => false;
+        private Pawn Reader => (Pawn)job.GetTarget(TargetIndex.B).Thing;
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            AddFailCondition(() => Reader.CurJob.def != MedicalBooksDefOf.BBLK_Job_PatientStory);
+            foreach (Toil toil in base.MakeNewToils())
+            {
+                yield return toil;
+            }
+        }
+        public override string GetReport()
+        {
+            return "BBLK_MedicalBooks_Listen".Translate(Reader.Named("PAWN"));
+        }
+    }
+}
--- 1.5/Source/Medical_Books/MedicalBooksDefOf.cs
+++ 1.5/Source/Medical_Books/MedicalBooksDefOf.cs
@@ -12,5 +12,9 @@ namespace Medical_Books
         }
         public static HediffDef BBLK_FirstAidBook_Knowledge;
         public static HediffDef BBLK_SurgicalBook_Knowledge;
+        public static InteractionDef BBLK_PatientStory;
+        public static JobDef BBLK_Job_PatientListen;
+        public static JobDef BBLK_Job_PatientStory;
+        public static TaleDef BBLK_PatientStory_Tale;
     }
 }
